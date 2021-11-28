7df57b1fe4a009ec0c075480a6d3775b3ad039d0
roxy <rdm3@williams.edu>
2021-11-28 13:26:11 -0500

    cleaner graphics and rebooting
    on top of refactoring the code, the bootloader now prompts the user to
    reboot by pressing 'r'. further work should focus on interpreting the
    partition table. need to learn more about approaches to bootloading.

--- bld.asm
+++ bld.asm
@@ -17,26 +17,55 @@
 ;;
 ;; <rdm3@williams.edu> Roxanne MacKinnon
 
-
+%define VIDEO_MODE 0x3
+%define VIDEO_WIDTH 25
+%define VIDEO_HEIGHT 80
+	
 	org 0x0
 	bits 16
 	jmp 0x7c0:start 	; set (cs,ip) = (0x7c0, start)
 start:	
 	mov bx, 0x7c0
 	mov ds, bx
-	mov bx, message
-	mov ah, 0x0e 		; prepare to print the char 'B'
-loop:
-	mov al, [bx]
-	and al, al
-	jz end
-	add bx, 1
+	
+set_video_mode:	
+	mov ah, 0x0 		; int 0 -> set video mode
+	mov al, VIDEO_MODE      ; 40 x 25 16 color text
 	int 0x10
-	jmp loop
-end:
+	
+clear_screen:
+	mov ah, 0x6 		; int 0x6 -> scroll window
+	mov al, 0 		; al=0 means blank the region
+	xor ch, ch 		; upper left row = 0
+	xor cl, 0		; leftmost column = 0
+	mov dh, VIDEO_HEIGHT-1 	; account for zero indexing with -1
+	mov dl, VIDEO_WIDTH-1 
+	int 0x10 		; clear window
+	
+prepare_message:	
+	mov bx, message
+	mov ah, 0x0e 		; ah 0xe -> print char to screen
+
+printstring:
+	mov al, [bx]		; get char
+	and al, al 		; if its zero, exit
+	jz wait_keypress
+	add bx, 1 		; incr string
+	int 0x10 		; print char
+	jmp printstring
+	
+wait_keypress: 			; wait for user to press 'r', then reboot
+	mov ah, 0x00
+	int 0x16
+	cmp al, 'r'
+	jne wait_keypress
+	
+reboot:
+	int 0x19
 	hlt
+	
 message:
-	db `hello, roxanne!\n\r`
+	db `Welcome to RunicOS!\n\rI haven't been implemented yet...\n\rPress 'r' to reboot: `
 	times 510-($-$$) db 0 
 	dw 0xaa55 		; magic numbers for boot segment
  
