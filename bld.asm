68b089d08a9d991e20b0e5f48ea4f8d077409873
roxy <rdm3@williams.edu>
2021-11-28 00:32:16 -0500

    switched address mode and prints hello message

--- bld.asm
+++ bld.asm
@@ -18,20 +18,25 @@
 ;; <rdm3@williams.edu> Roxanne MacKinnon
 
 
-	org 0x7c00
+	org 0x0
 	bits 16
-	jmp 0x0000:start 	; set (cs,ip) = (0, start)
+	jmp 0x7c0:start 	; set (cs,ip) = (0x7c0, start)
 start:	
-
-	xor bx, bx 		;
-	mov ds, bx 		; set ds = 0 in absolute memory
+	mov bx, 0x7c0
+	mov ds, bx
+	mov bx, message
 	mov ah, 0x0e 		; prepare to print the char 'B'
-	mov al, 'B'
-	mov [ds:0], BYTE 0xcd 	; at the absolute start of mem, 
-	mov [ds:1], BYTE 0x10 	; write the instructions "int 0x10, hlt"
-	mov [ds:2], BYTE 0xf4 	; this will print a character and then halt
-	jmp 0x0000:0 		; execute instruction at memory address 0
-				
+loop:
+	mov al, [bx]
+	and al, al
+	jz end
+	add bx, 1
+	int 0x10
+	jmp loop
+end:
+	hlt
+message:
+	db `hello, roxanne!\n\r`
 	times 510-($-$$) db 0 
 	dw 0xaa55 		; magic numbers for boot segment
  
