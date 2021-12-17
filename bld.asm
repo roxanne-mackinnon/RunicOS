086bb4f4b70bf83459219826d50038c9a32c04bd
roxy <rdm3@williams.edu>
2021-12-17 17:44:11 -0500

    code polish and commenting

--- bld.asm
+++ bld.asm
@@ -17,29 +17,6 @@
 ;;
 ;; <rdm3@williams.edu> Roxanne MacKinnon
 
-;; ptable entry format
-;; byte 0: 0x80 if good, else bad
-;; bytes 1-3: CHS address of first sector in partition
-;; byte 4: partition type
-;; bytes 5-7: CHS address of last sector in partition
-;; byte 8: LBA of first absoute sector in partition
-;; byte 0x0c: number of sectors in partition
-
-;;CHS addresses:
-;; byte 0: head number (from 0)
-;; byte 1: [c9, c8] s5-s0 (c9, c8 high bits of cylinder, s5-0 sector bits, indexed from 1)
-;; byte 2: c7-c0 (i think) low bits of cylinder address (from 0)
-
-;; int 0x13 call params:
-	; ah: function number
-	; al: num sects
-	; ch: cylinder number
-	; cl: sector number
-	; dh: head number
-	; dl: drive number
-	; es:bx: address of user buffer
-	; returns cf = 0 on success, cf = 1 on failure (carry flag)
-
 
 %define VIDEO_MODE 0x3
 %define VIDEO_WIDTH 25
@@ -50,13 +27,15 @@
 	bits 16
 	jmp 0x7c0:start 	; set (cs,ip) = (0x7c0, start)
 	
-	;; prepare to copy sector at 0x7c0:0x0000 to 0x060:0x0000
+	;; prepare to copy sector at 0x7c0:0x0 to 0x060:0x0
 start:
 	
 	;; set data segment to point to code segment 0x7c0:0x0
 	mov bx, 0x7c0
 	mov ds, bx
-	;;  store drive number (in dl) at the last byte of the 512 byte segment
+
+	
+	;;  store boot drive number for later use in BIOS calls
 	mov [ds:511], dl
 
 	;; clear interrupt and direction flags
@@ -64,15 +43,14 @@ start:
 	cld
 	
 	;; set video mode
-	mov ah, 0x0 		; int 0 -> set video mode
-	mov al, VIDEO_MODE      ; 40 x 25 16 color text
+	xor ah, ah 		; int 0 -> set video mode
+	mov al, VIDEO_MODE
 	int 0x10
 	
 	;; clear screen
 	mov ah, 0x6 		; int 0x6 -> scroll window
-	mov al, 0 		; al=0 means blank the region
+	xor al, al 		; al=0 means blank the region
 	xor ch, ch		; leftmost column = 0
-	xor cl, 0
 	mov dh, VIDEO_HEIGHT-1 	; account for zero indexing with -1
 	mov dl, VIDEO_WIDTH-1 
 	int 0x10 		; clear window
@@ -118,7 +96,6 @@ relocate:
 find_active_partition:
 	;; set up bx for looping through each of 4 entries
 	mov bx, ptable_start - 16
-	jmp find_active_partition0
 
 find_active_partition0:
 	;; go to next part table entry (or zeroeth one on first iteration)
@@ -132,15 +109,10 @@ find_active_partition0:
 	mov ah, [bx]
 	and ah,0x80
 	jz find_active_partition0
-	jmp load_part_parameters
 
-	;; print 'n' (used if there is no active partiion)
-no_active_partition:	
-	mov si, no_active_partition_message
-	call print_message
-	jmp prompt_reboot
+
 	
-	;; load partition parameters from active part. table entry to prepare
+	;; load partition parameters from active part table entry to prepare
 	;; for a disk read BIOS call
 load_part_parameters:
 	add bx,1  		;set bx to point to chs address of VBR sector
@@ -152,7 +124,7 @@ load_part_parameters:
 	mov bx, 0x7c0 		;set up location to which VBR should be read
 	mov es, bx
 	
-try_read_sector:
+	;; attempt to read sector into 0x7c0:0x0
 	xor bx, bx
 	mov ah, 0x2		;set bios call to DISK BIOS: READ DISK SECTORS
 	mov al, 1 		;set num sectors to read = 1
@@ -160,18 +132,14 @@ try_read_sector:
 
 	;; if carry flag is set (indicating error), print error message
 	adc bh, bh
-	jnz status_error
+	jnz bad_disk_read
 	
 	;; otherwise, print success message
-	jmp status_success
+	jmp successful_boot
 
 	
-	;; print 'b' to the screen and halt(used on bad disk read)
-status_error:
-	mov si, disk_failure_message
-	call print_message
-	jmp prompt_reboot
-	
+
+	;; generically print a message to the screen, argument in si
 print_message:	
 	lodsb
 	and al, al
@@ -183,26 +151,40 @@ print_message0:
 	int 0x10
 	jmp print_message
 
+	;; print 'n' (used if there is no active partiion)
+no_active_partition:	
+	mov si, no_active_partition_message
+	call print_message
+	jmp prompt_reboot
 	
-status_success:
+
+	;; print a welcome message to the screen (on successful partition read)
+successful_boot:
 	mov si, welcome_message
 	call print_message
+	jmp 0x7c0:0x0
+
+	;; print an error message to the screen (on disk read failure)
+bad_disk_read:	
+	mov si, disk_failure_message
+	call print_message
+	jmp prompt_reboot
 	
+	;; print message, then reboot
 prompt_reboot:
 	mov si, reboot_prompt_message
 	call print_message
 	jmp reboot_on_keypress
-	
+
+	;; Wait for user to press a key, then reboot
 reboot_on_keypress:
-	mov ah, 0x0
+	xor ah, ah
 	int 0x16
 	int 0x19
-	
-die:
 	hlt
 	
 
-
+	;; Messages used by bootloader
 no_active_partition_message:
 	db `No active partition found...\n\r\0`
 disk_failure_message:
@@ -211,16 +193,38 @@ welcome_message:
 	db `Welcome to RunicOS!\n\rDisk read success...\n\r\0`	
 reboot_prompt_message:	
 	db `Press any key to reboot: \0`
-	times 446-($-$$) db 0
-	db 0x80,0x00,0x02,0x00,   0x02,  0x00,0x02,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
-	db 0x00,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
-	db 0x00,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
-	db 0x00,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
+
+;; Partition table entry format:
+;; byte 0: 0x80 bit set (7 out of 0) if active, not set if inactive
+;; bytes 1-3: CHS address of first sector in partition
+;; byte 4: partition type
+;; bytes 5-7: CHS address of last sector in partition
+;; byte 8: LBA of first absoute sector in partition
+;; byte 0x0c: number of sectors in partition
+
+;;CHS addresses:
+;; byte 0: head number (from 0)
+;; byte 1: [c9, c8] s5-s0 (c9, c8 high bits of cylinder, s5-0 sector bits, indexed from 1)
+;; byte 2: c7-c0 (i think) low bits of cylinder address (from 0)
+
+;; int 0x13 call params:
+	; ah: function number
+	; al: num sects
+	; ch: cylinder number
+	; cl: sector number
+	; dh: head number
+	; dl: drive number
+	; es:bx: address of user buffer
+	; returns cf = 0 on success, cf = 1 on failure (carry flag)
+
+	;; Partition table
+	times 446-($-$$) db 0	; ensure we are starting at address 446
+	db 0x7f,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
+	db 0x7f,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
+	db 0x80,0x00,0x02,0x00,   0x02,  0x00,0x02,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00	
+	db 0x7f,0x00,0x01,0x00,   0x00,  0x00,0x01,0x00, 0x00,0x00,0x00,0x00, 0x01,0x00,0x00,0x00
 	times 510-($-$$) db 0 
 	dw 0xaa55 		; magic numbers for boot segment
-othersector:	
-	mov ah, 0x0e
-	mov al, 'n'
-	int 0x10
-	jmp othersector
+	
+	hlt
 	times 1023-($-$$) db 0
